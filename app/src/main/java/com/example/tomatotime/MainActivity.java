cffe683e713deca92b224d7c1954868d19f0f056
6Hacker9 <73985707+6Hacker9@users.noreply.github.com>
2021-11-23 12:08:02 +0200

    Update

--- app/src/main/java/com/example/tomatotime/MainActivity.java
+++ app/src/main/java/com/example/tomatotime/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tomatotime;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(5000,1000) {
+            @Override
+            public void onTick(long l) {
+                Toast.makeText(getApplicationContext(), "Loading... ("+ (l/1000) + ")", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onFinish() {
+                Intent go = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(go);
+            }
+        }.start();
     }
 }
